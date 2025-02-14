c4cb0e75575802b29686216f395ee313698f0287
Failure404 <33394205+Failure404@users.noreply.github.com>
2025-02-14 01:45:21 +0100

    Update run.sh

--- sox_play/run.sh
+++ sox_play/run.sh
@@ -3,14 +3,22 @@ set -e
 
 echo "Starting..."
 
+# Trap signals to handle unexpected termination
+trap 'echo "[Error] Terminated unexpectedly"; exit 1' SIGINT SIGTERM
+trap 'echo "[Error] Broken pipe (connection closed)"; continue' SIGPIPE
+
 # Read from STDIN aliases to play file
-while read -r input;
-do
-	echo "[Info] Parameters: $input"
-	# removing JSON stuff
-	input="$(echo "$input" | jq --raw-output '.')"
-	echo "[Info] Processed parameters: $input"
-	if ! msg="$(play $input)"; then
-		echo "[Error] Playing failed"
-	fi
+while true; do
+    while read -r input; do
+        echo "[Info] Parameters: $input"
+        
+        # Removing JSON stuff
+        input=$(echo "$input" | jq --raw-output '.')
+        #echo "[Info] Processed parameters: $input"
+        
+        # Playing the input file
+        if ! msg="$(play $input)"; then
+            echo "[Error] Playing failed"
+        fi
+    done
 done
